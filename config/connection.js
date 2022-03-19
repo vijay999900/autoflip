b0f90dda8b764f6fe97be7152bcc1e77263ccddd
vijay999900 <69893358+vijay999900@users.noreply.github.com>
2022-03-20 01:05:55 +0530

    Update connection.js
    new db

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,18 @@ var mysql  = require('mysql');
 
 
 //- Connection configuration
+// var db_config = {
+//   host     : 'sql6.freesqldatabase.com',
+//   user     : 'sql6470527',
+//   password : 'ziEDDHe5bZ',
+//   database:'sql6470527'
+// };
+
 var db_config = {
-  host     : 'sql6.freesqldatabase.com',
-  user     : 'sql6470527',
-  password : 'ziEDDHe5bZ',
-  database:'sql6470527'
+  host     : 'top9deals.com',
+  user     : 'top9ddyk_apadmin',
+  password : 'S@!E6a6a',
+  database:'top9ddyk_app_panel'
 };
 
 // var db_config = {
